8defc198706371ff6b61d78c82d17bf346c90ab1
Jason Mumbulla <jasonmumbulla@gmail.com>
2019-03-03 16:02:20 +1100

    Initial setup of cermineR.

---
+++ R/runCermine.R
@@ -0,0 +1,21 @@
+#' Run cermine on a PdF.
+#'
+#' Reads a PDF and converts it's content to a Journal Article Tag Suite (JATS) xml file.
+#'
+#' @param directory a directory containing the the PDF to convert.
+#'
+#' @return A vector containing the file reference to the JATS xml file.
+#'
+#' @author Jason Mumbulla, \email{jasonmumbulla@@gmail.com}
+#'
+#' @examples
+#'
+#' RunCermine(directory)
+#'
+#' @export
+#'
+pdf_to_convert <-function(directory)
+{
+  ajastfile <-c("afile")
+  return(ajastfile)
+}
